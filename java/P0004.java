129bc5b0c6675ae43536ac5166838d9d7a9d31a8
Audric <jyzhou15@uw.edu>
2017-02-04 17:13:28 -0800

    Problem 4 - Java

--- java/P0004.java
+++ java/P0004.java
@@ -4,6 +4,33 @@ from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
  */
-public class P0004 {
 
+import java.util.*;
+
+public class P0004 {
+    public static void main(String[] args) {
+        boolean done = false;
+        int max = 0;
+        for (int i = 100; i < 1000 && !done; i++) {
+            for (int j = i; j < 1000 && !done; j++) {
+                if (isPalindrome(i * j) && i * j > max) {
+                    max = i * j;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static boolean isPalindrome(int i) {
+        if (i > 10) {
+            int curr = i;
+            int reverse = 0;
+            while (curr > 0) {
+                reverse = reverse * 10 + curr % 10;
+                curr /= 10;
+            }
+            return i == reverse;
+        }
+        return true;
+    }
 }
