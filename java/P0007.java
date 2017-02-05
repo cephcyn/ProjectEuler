8db7aa5c61666841a5df785bd64c8168096bf23d
Audric <jyzhou15@uw.edu>
2017-02-04 19:13:38 -0800

    Problem 7 - Java

--- java/P0007.java
+++ java/P0007.java
@@ -4,10 +4,25 @@ that the 6th prime is 13.
 
 What is the 10 001st prime number?
  */
-
+import java.util.*;
 public class P0007 {
+    public static final int ST_PRIME = 10001;
     public static void main(String[] args) {
         // It's another prime problem... woohoo
-        
+        LinkedList<Integer> primes = new LinkedList<Integer>();
+        int curr = 2;
+        while (primes.size() < ST_PRIME) {
+            boolean prime = true;
+            for(int i : primes) {
+                if (curr % i == 0) {
+                    prime = false;
+                }
+            }
+            if (prime) {
+                primes.add(curr);
+            }
+            curr++;
+        }
+        System.out.println(primes.get(primes.size() - 1));
     }
 }
